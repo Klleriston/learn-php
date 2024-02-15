c40f47e278d1c6f24d473950cd25a3f8a4533667
Klleriston <Klleandrade@outlook.com>
2024-02-15 17:11:26 -0300

    add php repositorios com comentarios para fins educacionais he4rt

---
+++ a1.php
@@ -0,0 +1,26 @@
+<?php
+// echo "Hello" . PHP_EOL . "quis dizer oi" ;
+// var_dump("1" == 1)
+
+// $array = [
+//     "nome" => "Klleriston",
+//     "role" => "Software Developer",
+//     "age" => 20
+// ];
+
+// $arrayDois = [
+//     "favorite anime" => "One piece \o/",
+// ];
+
+// $all = $array + $arrayDois;
+
+// print_r($all);
+
+// $command = `cd ..`;
+// $command2 = `dir`;
+
+// echo $command;
+// echo $command2;
+echo "hello web"
+?>
+
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo "hello web";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>my html</p>
+    <img src="https://static.wikia.nocookie.net/otaku_encyclopedia/images/4/4c/Waifu_Image.jpg/revision/latest?cb=20200429154819" alt="">
+</body>
+</html>
